2750273a080a7afc908ff721e95b538a01210ece
Gabriel Valladão <37890829+gabrielvalladao@users.noreply.github.com>
2023-02-06 20:41:45 -0300

    Update2

--- src/containers/applications/apps/store.jsx
+++ src/containers/applications/apps/store.jsx
@@ -116,7 +116,7 @@ export const MicroStore = () => {
     if (!wnapp.hide && fetchState == 0) {
       var url = queryParams.get("customstore");
       if (!url)
-        url = "https://gabrielvalladao-hub.netlify.app/store/index.json";
+        url = "https://store.win11react.com/store/index.json";
 
       axios
         .get(url)
